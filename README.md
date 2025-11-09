1a5be1006e6604f5778bd3339c5437ccfd72c94b
Dan Jakob Ofer <dan@ofer.to>
2025-11-09 10:20:27 -0500

    chore(whitespace): fail trailing whitespace

---
+++ README.md
@@ -0,0 +1,9 @@
+# Me Not Whitespace
+
+Use in your GitHub Workflows by including,
+
+```yaml
+- uses: ofer987/me-not-whitespace@v1
+  with:
+    BASE_REF: ${{ github.base_ref }}
+```
---
+++ lib/me_not_whitespace.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'active_support'
+require 'active_support/core_ext'
+
+require_relative 'me_not_whitespace/version'
+
+module MeNotWhitespace
+  class Error < StandardError; end
+  # Your code goes here...
+end
